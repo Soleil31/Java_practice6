16d198a589eb6ba1b4582b6ec01513a077e3b247
Ivan <matveev92iv@icloud.com>
2021-11-21 17:52:18 +0300

    Initial commit

---
+++ src/com/metanit/Student.java
@@ -0,0 +1,34 @@
+package com.metanit;
+import java.lang.*;
+
+public class Student {
+    private static int count = 0;
+    private int id;
+    private int score;
+    public Student(int score) {
+        count++;
+        id = count;
+        this.score = score;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", score=" + score +
+                '}';
+    }
+}
+
